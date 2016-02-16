63e308f6684e0603a10b6954c2155d4d73c28e92
tobermanar <tobermanar@hotmail.fr>
2016-02-16 11:22:03 +0100

    Création de la calsse point
    Création de la classe point contenant la structure lien (voisins)

---
+++ Taquin/point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Taquin
+{
+    struct lien
+    {
+        private string nom_Voisin;
+        private int distance;
+         
+        public string Nom_Voisin
+        {
+            get { return nom_Voisin;}
+            set{ nom_Voisin = value;}
+        }
+        public int Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+    }
+    class point
+    {
+        string nom_Point;
+        List<lien> voisin;
+
+        public string Nom_Voisin
+        {
+            get { return nom_Point; }
+            set { nom_Point = value; }
+        }
+        public List<lien> Voisin
+        {
+            get { return voisin; }
+        }
+        public void Point(string nom, List<lien> voisins)
+        {
+            nom_Point = nom;
+            voisin = voisins;
+        }
+    }
+
+
+}
