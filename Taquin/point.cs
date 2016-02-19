2ed91e58b4c2b74203c47197d8262e0374d0c182
tobermanar <tobermanar@hotmail.fr>
2016-02-19 15:36:22 +0100

    Création monde, point, lien définitif

--- Taquin/point.cs
+++ Taquin/point.cs
@@ -8,13 +8,8 @@ namespace projettaquin
     struct lien
     {
         private string _nomVoisin;
-        private int _distance;
-<<<<<<< HEAD
-         
-        public string Nom_Voisin
-=======
+        private int _distance;   
         public string NomVoisin
->>>>>>> refs/remotes/origin/master
         {
             get { return _nomVoisin;}
             set{ _nomVoisin = value;}
@@ -23,27 +18,20 @@ namespace projettaquin
         {
             get { return _distance; }
             set { _distance = value; }
-<<<<<<< HEAD
         }
         public lien(string nom, int dist)
         {
             _nomVoisin = nom;
             _distance = dist;
-=======
->>>>>>> refs/remotes/origin/master
         }
     }
     class Point
     {
-<<<<<<< HEAD
         string nom_Point;
         List<lien> list_voisins;
         bool ferme;
-=======
         private string _nomPoint;
         private List<lien> _list_Voisins;
->>>>>>> refs/remotes/origin/master
-
         public string NomVoisin
         {
             get { return _nomPoint; }
@@ -56,7 +44,6 @@ namespace projettaquin
         }
         public Point(string nom, List<lien> voisins)
         {
-<<<<<<< HEAD
             nom_Point = nom;
             list_voisins = voisins;
             ferme = false;
@@ -66,11 +53,6 @@ namespace projettaquin
             nom_Point = nom;
             list_voisins = voisins;
             ferme = farm;
-        }     
-=======
-            _nomPoint = nom;
-            _list_Voisins = voisins;
         }
->>>>>>> refs/remotes/origin/master
     }
 }
